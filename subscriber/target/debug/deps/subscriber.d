ba6d9058e34d09b5013228ec2aaf7f1bf2cab325
Maira <mairaazsh@gmail.com>
2026-05-05 04:44:05 +0700

    Initial subscriber code.

---
+++ subscriber/target/debug/deps/subscriber.d
@@ -0,0 +1,5 @@
+D:\UI\Fasilkom\Semester 4\AdvProg\Tutorial\tutorial8-subscriber\subscriber\target\debug\deps\subscriber.d: src\main.rs
+
+D:\UI\Fasilkom\Semester 4\AdvProg\Tutorial\tutorial8-subscriber\subscriber\target\debug\deps\subscriber.exe: src\main.rs
+
+src\main.rs:
