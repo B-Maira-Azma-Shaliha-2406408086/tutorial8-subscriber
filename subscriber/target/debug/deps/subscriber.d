e5938b5963ced6fb270aace833706e28976e529f
Maira <mairaazsh@gmail.com>
2026-05-11 17:20:31 +0700

    Simulation slow subscriber.

--- subscriber/target/debug/deps/subscriber.d
+++ subscriber/target/debug/deps/subscriber.d
@@ -1,5 +1,5 @@
-D:\UI\Fasilkom\Semester 4\AdvProg\Tutorial\tutorial8-subscriber\subscriber\target\debug\deps\subscriber.d: src\main.rs
+D:\UI\Fasilkom\Semester 4\AdvProg\Tutorial\Module 9\subscriber\target\debug\deps\subscriber.d: src\main.rs
 
-D:\UI\Fasilkom\Semester 4\AdvProg\Tutorial\tutorial8-subscriber\subscriber\target\debug\deps\subscriber.exe: src\main.rs
+D:\UI\Fasilkom\Semester 4\AdvProg\Tutorial\Module 9\subscriber\target\debug\deps\subscriber.exe: src\main.rs
 
 src\main.rs:
--- subscriber/target/debug/subscriber.d
+++ subscriber/target/debug/subscriber.d
@@ -1,1 +1,1 @@
-D:\UI\Fasilkom\Semester\ 4\AdvProg\Tutorial\tutorial8-subscriber\subscriber\target\debug\subscriber.exe: D:\UI\Fasilkom\Semester\ 4\AdvProg\Tutorial\tutorial8-subscriber\subscriber\src\main.rs
+D:\UI\Fasilkom\Semester\ 4\AdvProg\Tutorial\Module\ 9\subscriber\target\debug\subscriber.exe: D:\UI\Fasilkom\Semester\ 4\AdvProg\Tutorial\Module\ 9\subscriber\src\main.rs
